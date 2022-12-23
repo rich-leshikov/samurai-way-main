f536bf034a268b1d0ce7f2730c426e8ee0da4334
Richard Leshik <richardleshik@gmail.com>
2022-12-23 15:13:02 +0300

    add Dialogs component

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import {Header} from './components/Header/Header';
 import {Navbar} from './components/Navbar/Navbar';
 import {Profile} from './components/Profile/Profile';
 import {Footer} from './components/Footer/Footer';
+import {Dialogs} from './components/Dialogs/Dialogs';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Navbar/>
-      <Profile/>
+      <div className="app-wrapper-css">
+        <Profile/>
+        {/*<Dialogs/>*/}
+      </div>
       <Footer/>
     </div>
   );
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from "./Dialogs.module.css"
+
+type DialogsPropsType = {
+
+}
+
+export function Dialogs(props: DialogsPropsType) {
+  return (
+    <div className={s.dialogs}>
+      Dialogs
+    </div>
+  );
+}
