4693951176bf3d6e048cad2f47ca802abf7d6083
Richard Leshik <richardleshik@gmail.com>
2022-12-24 01:05:06 +0300

    add NavLinks to Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import s from "./Dialogs.module.css"
+import {NavLink} from 'react-router-dom';
+import s from './Dialogs.module.css'
 
-type DialogsPropsType = {
-
-}
+type DialogsPropsType = {}
 
 export function Dialogs(props: DialogsPropsType) {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs__items}>
-        <div className={`${s.dialog} ${s.active}`}>Dimych</div>
-        <div className={s.dialog}>Victor</div>
-        <div className={s.dialog}>Andrey</div>
-        <div className={s.dialog}>Sasha</div>
-        <div className={s.dialog}>Sveta</div>
-        <div className={s.dialog}>Valera</div>
+        <div className={`${s.dialog} ${s.active}`}>
+          <NavLink to={'/dialogs/1'} activeClassName={s.active}>Dimych</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to={'/dialogs/2'} activeClassName={s.active}>Victor</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to={'/dialogs/3'} activeClassName={s.active}>Andrey</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to={'/dialogs/4'} activeClassName={s.active}>Sasha</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to={'/dialogs/5'} activeClassName={s.active}>Sveta</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to={'/dialogs/6'} activeClassName={s.active}>Valera</NavLink>
+        </div>
       </div>
       <div className={s.dialogs__chats}>
         <div className={s.chat}>Hi!</div>
