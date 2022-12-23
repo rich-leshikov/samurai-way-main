4b08b3bdfd85879cbf14bdfc3df4a17cb71540c2
Richard Leshik <richardleshik@gmail.com>
2022-12-23 23:53:39 +0300

    add content to Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -8,7 +8,19 @@ type DialogsPropsType = {
 export function Dialogs(props: DialogsPropsType) {
   return (
     <div className={s.dialogs}>
-      Dialogs
+      <div className={s.dialogs__items}>
+        <div className={`${s.dialog} ${s.active}`}>Dimych</div>
+        <div className={s.dialog}>Victor</div>
+        <div className={s.dialog}>Andrey</div>
+        <div className={s.dialog}>Sasha</div>
+        <div className={s.dialog}>Sveta</div>
+        <div className={s.dialog}>Valera</div>
+      </div>
+      <div className={s.dialogs__chats}>
+        <div className={s.chat}>Hi!</div>
+        <div className={s.chat}>What's good?</div>
+        <div className={s.chat}>Yo!</div>
+      </div>
     </div>
   );
 }
