5a67824c9f44a2fe3843457b6704349fae8176a3
Richard Leshik <richardleshik@gmail.com>
2023-02-01 22:21:50 +0300

    refactor profile reducer: add state copy returning, reverse posts sequence like feed

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -12,26 +12,30 @@ export const addPostAC = () => ({type: ADD_POST} as const)
 let initialState: ProfilePageType = {
   newPostFromTextarea: '',
   posts: [
-    {id: v1(), message: 'Hello!', likesCount: 3},
-    {id: v1(), message: 'What a nice day!', likesCount: 5},
     {id: v1(), message: 'Today I\'m playing guitar!', likesCount: 6},
+    {id: v1(), message: 'What a nice day!', likesCount: 5},
+    {id: v1(), message: 'Hello!', likesCount: 3},
   ]
 }
 
 export const profileReducer = (state: ProfilePageType = initialState, action: ActionsType): ProfilePageType => {
   switch (action.type) {
     case UPDATE_POST_TEXTAREA:
-      state.newPostFromTextarea = action.newText
-      return state
+      return {
+        ...state,
+        newPostFromTextarea: action.newText
+      }
     case ADD_POST:
       const newPost: PostsType = {
         id: v1(),
         message: state.newPostFromTextarea,
         likesCount: 0
       }
-      state.posts.push(newPost)
-      state.newPostFromTextarea = ''
-      return state
+      return {
+        ...state,
+        newPostFromTextarea: '',
+        posts: [newPost, ...state.posts]
+      }
     default:
       return state
   }
