d7fa03a00e91c7387ba472c135721db40482a58b
Richard Leshik <richardleshik@gmail.com>
2023-04-30 12:19:55 +0300

    add useEffect to ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from './ProfileStatus.module.css'
 import {ThunkType} from '../../../../redux/redux-store';
 
@@ -7,15 +7,15 @@ type ProfileStatusPropsType = {
   status: string
   changeStatus: (status: string) => ThunkType
 }
-// type ProfileStatusStateType = {
-//   editMode: boolean
-//   status: string
-// }
 
 
 export const ProfileStatus = (props: ProfileStatusPropsType) => {
   const [editMode, setEditMode] = useState<boolean>(false)
   const [status, changeStatus] = useState<string>(props.status)
+
+  useEffect(() => {
+    changeStatus(props.status)
+  }, [props.status])
 
   const activateEditMode = () => setEditMode(true)
   const deactivateEditMode = () => {
@@ -23,12 +23,6 @@ export const ProfileStatus = (props: ProfileStatusPropsType) => {
     props.changeStatus(status)
   }
   const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => changeStatus(e.currentTarget.value)
-
-  // const componentDidUpdate = (prevProps: Readonly<ProfileStatusPropsType>, prevState: Readonly<ProfileStatusStateType>, snapshot ?: any) => {
-  //   if (prevProps.status !== props.status) {
-  //     changeStatus(props.status)
-  //   }
-  // }
 
   return (
     <div className={s.profileStatus}>
